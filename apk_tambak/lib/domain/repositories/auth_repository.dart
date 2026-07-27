9f614a2512705671212908098ae86efc4fdf54d7
DAVANA YUDIST <davayudist@gmail.com>
2026-07-27 22:21:06 +0700

    Remove apk_tambak directory from git tracking

--- apk_tambak/lib/domain/repositories/auth_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/user_entity.dart';
-
-abstract class AuthRepository {
-  Future<UserEntity> login(String email, String password);
-  Future<UserEntity> register(String name, String email, String password, int roleId);
-  Future<void> logout();
-  Future<bool> isLoggedIn();
-}
